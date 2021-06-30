a657c1fe50add3569be26b124b1de214de586c7d
athirasarman <kaithakkottuathira@gmail.com>
2021-06-30 14:04:57 +0530

    modules created

--- BooksRUs/src/app/app-routing.module.ts
+++ BooksRUs/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, { path: 'analytics', loadChildren: () => import('./analytics/analytics.module').then(m => m.AnalyticsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
