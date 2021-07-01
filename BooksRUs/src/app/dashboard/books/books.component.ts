e22e90f1ca16a976f4f31e655da48e475451cb85
athirasarman <kaithakkottuathira@gmail.com>
2021-07-01 18:10:02 +0530

    no results found display functionality added

--- BooksRUs/src/app/dashboard/books/books.component.ts
+++ BooksRUs/src/app/dashboard/books/books.component.ts
@@ -12,19 +12,24 @@ import { Books } from '../../books';
 export class BooksComponent implements OnDestroy{
   bookList:Observable<Books[]>=of([]);
   books: Subscription;
+  display=true;
   constructor(private fb: FormBuilder,
     private sharedSubjectsService:SharedSubjectsService) 
   {
     this.books=this.sharedSubjectsService.books.subscribe(data=>{
       this.bookList=of(data);
-      
+      if(data.length==0)
+        this.display=false;
     });
+   
   }
 
+ 
 
   ngOnDestroy() {
         // unsubscribe to ensure no memory leaks
         this.books.unsubscribe();
-    
 }
+    
+
 }
